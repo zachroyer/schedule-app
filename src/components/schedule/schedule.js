a290f7038c6bcdee2bb9a154f21a91c9aba806ce
Zach Royer <zach.royer14@gmail.com>
2018-08-02 15:18:57 -0600

    built schedule course component and placed on grid

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
 import Gradient from '../gradient';
+import ScheduleCourse from './scheduleCourse';
 
 class Schedule extends Component {
     render() {
         return (
             <div className='schedule'>
                 <h1 className='schedule__title'>Course Schedule</h1>
+                <ScheduleCourse title={'Problem Solving'}/>
+                <ScheduleCourse title={'Problem Solving'}/>
+                <ScheduleCourse title={'Problem Solving'}/>
                 <Gradient/>
             </div>
         )
