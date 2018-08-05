c318842d232901e5c64549cda343a4cbf39dfec1
Zach Royer <zach.royer14@gmail.com>
2018-08-05 13:41:17 -0600

    created toggleCourse action creater and reducer and connected libraryCourse component to redux to call that within action.js

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../../actions';
 
 import Icon from '../icon';
 import Arrow from '../arrow';
 import Action from '../action';
+
 
 class LibraryCourse extends Component {
     render() {
@@ -13,7 +16,7 @@ class LibraryCourse extends Component {
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <Arrow className='library-course__arrow'/>
-                <Action className="library-course__action"/>
+                <Action onClick={this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
                 <div className='library-course__description'>
                     <label>Course Description</label>
                     <p>{this.props.description}</p>
@@ -23,4 +26,4 @@ class LibraryCourse extends Component {
     }
 }
 
-export default LibraryCourse;
+export default connect(null, actions )(LibraryCourse);
