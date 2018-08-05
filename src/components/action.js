1959208c3e47caa2906a94162de56bc51015b669
Zach Royer <zach.royer14@gmail.com>
2018-08-05 16:11:52 -0600

    used react acnimate height library to animate paragraph height

--- src/components/action.js
+++ src/components/action.js
@@ -9,9 +9,9 @@ class Action extends Component {
         this.status = false;
     }
 
-    handleAction = function() {
+    handleAction = function () {
         this.props.onClick()
-        if(!this.status) {
+        if (!this.status) {
             document.getElementById(this.id).classList.add('action-remove');
         } else {
             document.getElementById(this.id).classList.remove('action-remove');
@@ -26,7 +26,7 @@ class Action extends Component {
                 id={this.id}
                 onClick={() => this.handleAction()}
                 className={`${this.props.className} action`}>
-            
+
             </a>
         )
         // return (
