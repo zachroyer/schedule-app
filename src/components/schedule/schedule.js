01c3c7a6e14426e5c2b30d8729761f56522fe350
Zach Royer <zach.royer14@gmail.com>
2018-08-05 14:07:27 -0600

    finished functionality with the exception of a couple soon to be found bugs

--- src/components/schedule/schedule.js
+++ src/components/schedule/schedule.js
@@ -11,18 +11,20 @@ class Schedule extends Component {
         const data = this.props.courses
 
         return data.map((course, index) => {
-            if(course.enrolled) {
+            if(course.enrolled && this.amountEnrolled != 5) {
+                this.amountEnrolled++;
                 return <ScheduleCourse {...course} key={index}/>
             }
         })
     }
 
     render() {
+        this.amountEnrolled = 0
         return (
             <div className='schedule'>
                 <h1 className='schedule__title'>Course Schedule</h1>
                 { this.renderCourses()}
-                <ProgressTracker/>
+                <ProgressTracker amountPossible={5} amountEnrolled={this.amountEnrolled}/>
                 <Gradient/>
             </div>
         )
