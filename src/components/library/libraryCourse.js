7b611babb3495a882e3be57d578e7c99eb5b3055
Zach Royer <zach.royer14@gmail.com>
2018-08-05 15:23:45 -0600

    rendering description based on arrow status

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -8,6 +8,30 @@ import Action from '../action';
 
 
 class LibraryCourse extends Component {
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            status: true
+        }
+    }
+
+    renderDescription = function() {
+        if (!this.state.status) {
+            return (
+                <div className='library-course__description'>
+                    <label>Course Description</label>
+                    <p>{this.props.description}</p>
+                </div>
+            )
+        }
+    }.bind(this)
+
+    handleCallback = function(status) {
+        this.setState({ status })
+    }.bind(this)
+
     render() {
         return (
             <div className='library-course'>
@@ -16,12 +40,9 @@ class LibraryCourse extends Component {
                     { Icon('fas fa-check', 'library-course__icon') }
                 </div>
                 <div className='library-course__line'></div>
-                <Arrow className='library-course__arrow'/>
+                <Arrow callback={status => this.handleCallback(status) } id={this.props.id} className='library-course__arrow'/>
                 <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="library-course__action"/>
-                <div className='library-course__description'>
-                    <label>Course Description</label>
-                    <p>{this.props.description}</p>
-                </div>
+                {this.renderDescription() }
             </div>
         )
     }
